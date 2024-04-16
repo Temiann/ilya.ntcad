3c240aedb7e7f9efdce9a7e702e22275a65e62a2
Temiann <temiann36@gmail.com>
2024-04-16 14:48:34 +0300

    added include areas

---
+++ local/templates/mainPage/include/template/contacts.php
@@ -0,0 +1,3 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<a class="header-main__contacts-tel" href="tel:+78614725800">8 (86147) 2-58-00</a>
+<a class="header-main__contacts-mail" href="mailto:zdor_prod@mail.ru">zdor_prod@mail.ru</a>
---
+++ local/templates/mainPage/include/template/privacy.php
@@ -0,0 +1,2 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<a class="header-main__contacts-policy" href="#">Политика конфиденциальности</a>
